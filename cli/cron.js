622d513e5726b0f360b47fddba90ce9ca586dc2a
Up Chen <wallat@gmail.com>
2021-09-24 21:36:04 +0800

    Update to the production cron.

--- cli/cron.js
+++ cli/cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const logger = require('../lib/logger');
 const AllChecks = require('../models/McChecks/AllChecks')
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('*/10 * * * *', async () => {
   console.log('cron running', new Date())
 }, {
   scheduled: true,
@@ -12,9 +12,8 @@ cron.schedule('* * * * *', async () => {
 // Note. The server would treat this as UTC time. 
 // 02:00 UTC = 10:00+800
 // 08:00 UTC = 14:00+800
-cron.schedule('0,5,10,15,20,25,30,35,40,45,50,55 17,18,19 * * *', async () => {
+cron.schedule('0 10,16 * * *', async () => {
   console.log(`Run All Checks at ${new Date()}`)
-  logger.info(`Run All Checks at ${new Date()}`)
   await AllChecks()
 }, {
   scheduled: true,
