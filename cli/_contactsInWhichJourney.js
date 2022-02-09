be546da1fca2fdb3de58c1ebc6d77fcc0840f0c6
Up Chen <wallat@gmail.com>
2022-02-09 16:20:18 +0800

    Added journey stage error monitor.
    Remove stash files.

--- cli/_contactsInWhichJourney.js
+++ cli/_contactsInWhichJourney.js
@@ -5,31 +5,41 @@ const Notifier = require('../lib/Notifier.js')
 
 async function main () {
   let mcbase = new MCBase({
-    clientId: process.env.MC_TW_CLIENTID,
-    clientSecret: process.env.MC_TW_CLIENTSECRET,
-    subDomain: process.env.MC_TW_SUBDOMAIN,
-    accountId: process.env.MC_TW_ACCOUNTID,
+    clientId: process.env.MC_HK_CLIENTID,
+    clientSecret: process.env.MC_HK_CLIENTSECRET,
+    subDomain: process.env.MC_HK_SUBDOMAIN,
+    accountId: process.env.MC_HK_ACCOUNTID,
   })
   await mcbase.doAuth()
   let mcJourney = mcbase.factory('Journey')
 
   // contacts keys to find
-  let contactKeys = ["0032u00000DpLe0AAF", "0032u00000Dpj8CAAR", "0032u00000DpleYAAR"]
+  let contactKeys = [
+    '0032u00000HqoouAAB'
+  ]
 
-  let r = await mcJourney.contactInWhichJourneys(contactKeys)
-  console.log('r', JSON.stringify(r,null,2))
+  
+  // 
 
-  let foundContactMemberships = _.get(r, 'results.contactMemberships', [])    
+  let foundContactMemberships = []
+
+  for (let i = 0; i < contactKeys.length; i+=50) {
+    const element = contactKeys[i];
+
+    let r = await mcJourney.contactInWhichJourneys(contactKeys.slice(i, i+50))
+    foundContactMemberships = foundContactMemberships.concat(_.get(r, 'results.contactMemberships', []))
+
+    console.log('r', JSON.stringify(r,null,2))
+  }
+
+
   for (let i = 0; i < foundContactMemberships.length; i++) {
     let contactKey = foundContactMemberships[i].contactKey;
     let definitionKey = foundContactMemberships[i].definitionKey;
 
     let j = await mcJourney.findByKey(definitionKey)
 
-    console.log("contactKey", contactKey)
-    console.log(` is in ${j['name']} v${j['version']}`)
-
-    
+    console.log(`${contactKey} is in ${j['name']} v${j['version']}`)
   }
 }
 
