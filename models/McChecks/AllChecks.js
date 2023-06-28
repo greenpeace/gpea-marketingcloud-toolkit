7043c3b4ac3ec1315f261f8ac82882b1ccd62cab
Up Chen <hi.upchen@gmail.com>
2023-06-28 11:00:15 +0800

    Wrap Journeys errors for MetGoalCriteria, Email Address for this subscriber has invalid syntax

--- models/McChecks/AllChecks.js
+++ models/McChecks/AllChecks.js
@@ -1,23 +1,23 @@
-const MCBase = require('../MarketingCloud/Base')
-const AutomationCheck = require("./AutomationChecks")
-const JourneyNumAudiencesChecks = require("./JourneyNumAudiencesChecks")
-const JourneyStageErrorChecks = require("./JourneyStageErrorChecks")
-const SingleSendEmailChecks = require("./SingleSendEmailChecks")
-const logger = require('../../lib/logger');
-const Notifier = require('../../lib/Notifier.js')
-const format = require('date-fns/format')
-const axios = require('axios');
-require('dotenv').config()
+const MCBase = require("../MarketingCloud/Base");
+const AutomationCheck = require("./AutomationChecks");
+const JourneyNumAudiencesChecks = require("./JourneyNumAudiencesChecks");
+const JourneyStageErrorChecks = require("./JourneyStageErrorChecks");
+const SingleSendEmailChecks = require("./SingleSendEmailChecks");
+const logger = require("../../lib/logger");
+const Notifier = require("../../lib/Notifier.js");
+const format = require("date-fns/format");
+const axios = require("axios");
+require("dotenv").config();
 
 async function main() {
-  let markets = ["TW", "HK", "KR"]
+  let markets = ["TW", "HK", "KR"];
 
   for (let i = 0; i < markets.length; i++) {
     const market = markets[i];
-    logger.debug(`Start to check market ${market}`)
+    logger.debug(`Start to check market ${market}`);
 
-    let mcbase
-    let journeyRules = []
+    let mcbase;
+    let journeyRules = [];
 
     if (market === "TW") {
       // init marketing cloud settings
@@ -26,7 +26,7 @@ async function main() {
         clientSecret: process.env.MC_TW_CLIENTSECRET,
         subDomain: process.env.MC_TW_SUBDOMAIN,
         accountId: process.env.MC_TW_ACCOUNTID,
-      })
+      });
     } else if (market === "HK") {
       // init marketing cloud settings
       mcbase = new MCBase({
@@ -34,7 +34,7 @@ async function main() {
         clientSecret: process.env.MC_HK_CLIENTSECRET,
         subDomain: process.env.MC_HK_SUBDOMAIN,
         accountId: process.env.MC_HK_ACCOUNTID,
-      })
+      });
     } else if (market === "KR") {
       // init marketing cloud settings
       mcbase = new MCBase({
@@ -42,60 +42,59 @@ async function main() {
         clientSecret: process.env.MC_KR_CLIENTSECRET,
         subDomain: process.env.MC_KR_SUBDOMAIN,
         accountId: process.env.MC_KR_ACCOUNTID,
-      })
+      });
     }
 
     if (!mcbase) {
-      throw new Error(`Undefined market ${market}`)
+      throw new Error(`Undefined market ${market}`);
     }
 
     // fetch journey rules
-    let url = `${process.env.JOURNEY_SCHEMA_ENPOINT}?q={"status":"Enable", "market":"${market}"}`
-    logger.debug(`Retrieving the journey settings for ${market}: ${url}`)
-    let response = await axios.get(url)
+    let url = `${process.env.JOURNEY_SCHEMA_ENPOINT}?q={"status":"Enable", "market":"${market}"}`;
+    logger.debug(`Retrieving the journey settings for ${market}: ${url}`);
+    let response = await axios.get(url);
     journeyRules = response.data.records ? response.data.records : [];
     // logger.debug(`Use journey ruels ${JSON.stringify(journeyRules, null, 2)}`)
 
     // auth with marketing cloud
-    await mcbase.doAuth()
+    await mcbase.doAuth();
 
-    let allErros = []
+    let allErros = [];
     let rs;
 
-    rs = await AutomationCheck(mcbase)
-    allErros = allErros.concat(rs.errors)
+    rs = await AutomationCheck(mcbase);
+    allErros = allErros.concat(rs.errors);
 
-    rs = await JourneyNumAudiencesChecks(mcbase, journeyRules)
-    allErros = allErros.concat(rs.errors)
+    rs = await JourneyNumAudiencesChecks(mcbase, journeyRules);
+    allErros = allErros.concat(rs.errors);
     // console.log('journey errors', errors)
-    
-    rs = await SingleSendEmailChecks(mcbase)
-    allErros = allErros.concat(rs.errors)
+
+    rs = await SingleSendEmailChecks(mcbase);
+    allErros = allErros.concat(rs.errors);
     // console.log('send errors', rs.errors)
 
     if (allErros.length) {
-      allErros.unshift({ message: `\n　\n🙈🙈🙈 ${market}: *${allErros.length}* errors on *${format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx")}*\n` })
-      Notifier.sendToSlack(allErros.map(e => e.message).join("\n"))
+      allErros.unshift({ message: `\n　\n🙈🙈🙈 ${market}: *${allErros.length}* errors on *${format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx")}*\n` });
+      Notifier.sendToSlack(allErros.map((e) => e.message).join("\n"));
     } else {
-      Notifier.sendToSlack(`👍👍👍 ${market}: No Errors today`)
+      Notifier.sendToSlack(`👍👍👍 ${market}: No Errors today`);
     }
 
-    let allWarnings = []
-    rs = await JourneyStageErrorChecks(mcbase)
-    allWarnings = allWarnings.concat(rs.errors)
+    let allWarnings = [];
+    rs = await JourneyStageErrorChecks(mcbase);
+    allWarnings = allWarnings.concat(rs.errors);
     // console.log('journey stage errors', rs.errors)
-
     // console.log(JSON.stringify(rs.errors, null, 4));
 
     if (allWarnings.length) {
-      allWarnings.unshift({ message: `\n　\n 🙋🙋🙋 ${market}: *${allWarnings.length}* warnings on *${format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx")}*\n` })
-      Notifier.sendToSlack(allWarnings.map(e => e.message).join("\n"))
+      allWarnings.unshift({ message: `\n　\n 🙋🙋🙋 ${market}: *${allWarnings.length}* warnings on *${format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx")}*\n` });
+      Notifier.sendToSlack(allWarnings.map((e) => e.message).join("\n"));
     }
   }
 }
 
 // (async () => {
-//   var text = await main(); 
+//   var text = await main();
 // })()
 
-module.exports = main
+module.exports = main;
--- models/McChecks/JourneyStageErrorChecks.js
+++ models/McChecks/JourneyStageErrorChecks.js
@@ -1,118 +1,128 @@
-const MCBase = require('../../models/MarketingCloud/Base')
-const logger = require('../../lib/logger');
-const _ = require("lodash")
-const parser = require('cron-parser');
-const differenceInDays = require('date-fns/differenceInDays')
-const format = require('date-fns/format')
+const MCBase = require("../../models/MarketingCloud/Base");
+const logger = require("../../lib/logger");
+const _ = require("lodash");
+const parser = require("cron-parser");
+const differenceInDays = require("date-fns/differenceInDays");
+const format = require("date-fns/format");
 
 /**
  * To generate the viewable error string for each stage error item
- * 
+ *
  * @param {Object} item {result: {messages:[{parameters, }]}}
- * @returns 
+ * @returns
  */
 const genItemErrorMsg = (item) => {
-  let errMsgs = []
+  let errMsgs = [];
   // console.log('------')
   // console.log('item', item)
 
-  let resultMessages = _.get(item, 'result.messages', [])
+  let resultMessages = _.get(item, "result.messages", []);
   for (let messageIdx = 0; messageIdx < resultMessages.length; messageIdx++) {
     const aMessage = resultMessages[messageIdx];
     // console.log('aMessage', aMessage)
 
     // format the message string
-    let sMessage = aMessage.message
+    let sMessage = aMessage.message;
     if (aMessage.parameters) {
       for (const [k, v] of Object.entries(aMessage.parameters)) {
-        sMessage = sMessage.replace('{' + k + '}', v)
-      }  
+        sMessage = sMessage.replace("{" + k + "}", v);
+      }
     }
 
     // if (aMessage.level === "Error") {
-      errMsgs.push(`contactKey:${item.contactKey} ${item.activityName || ""}: \`${sMessage}\``)
+    errMsgs.push(`contactKey:${item.contactKey} ${item.activityName || ""}: \`${sMessage}\``);
     // }
   }
   // console.log(`errMsgs`, errMsgs)
 
-  return errMsgs.join("\n")
-}
+  return errMsgs.join("\n");
+};
 
 /**
  * Check journeys based on the rules
- * 
- * @param {MCBase} mcbase 
+ *
+ * @param {MCBase} mcbase
  * @param {array} rules [{name, cumulativePopulation, numContactsCurrentlyInJourney, numContactsAcceptedIn30Days}]
- * @returns 
+ * @returns
  */
 const check = async (mcbase, rules) => {
   // for journeys
-  let mcJourney = mcbase.factory('Journey')
-  let errors = []
+  let mcJourney = mcbase.factory("Journey");
+  let errors = [];
 
-  let r = await mcJourney.getJourneyErrorHistory(inDays=1)
-  let journeyErrorGroup = _.groupBy(r.items, 'definitionName')
+  let r = await mcJourney.getJourneyErrorHistory((inDays = 1));
+  let journeyErrorGroup = _.groupBy(r.items, "definitionName");
 
   _.forEach(journeyErrorGroup, (items, journeyName) => {
-    let thisErrMsg = ''
+    let thisErrMsg = "";
     let errorTypeGroups = {
-      'ContactPreviouslyInSameInteraction': [],
-      'ListDetective': [],
-      'SuppressionLogic': [],
-      'InvalidEmail': [],
-      'MetExitCriteria': [],
-      'CurrentlyWaitingInSameInteraction': [],
-      'Others': [],
-    }
+      "ContactPreviouslyInSameInteraction": [],
+      "ListDetective": [],
+      "SuppressionLogic": [],
+      "GlobalUnsubscribeList": [],
+      "InvalidEmail": [],
+      "MetExitCriteria": [],
+      "MetGoalCriteria": [],
+      "CurrentlyWaitingInSameInteraction": [],
+      "Others": [],
+    };
 
     for (let i = 0; i < items.length; i++) {
-      const item = items[i]
-      let resultMessages = _.get(item, 'result.messages', [])
+      const item = items[i];
+      let resultMessages = _.get(item, "result.messages", []);
 
-      if (resultMessages.some(m => m.message.indexOf('excluded by List Detective') >=0)) {
-        errorTypeGroups.ListDetective.push(item)
-      } else if (resultMessages.some(m => m.message.indexOf('excluded by Suppression logic') >=0)) {
-        errorTypeGroups.SuppressionLogic.push(item)
-      } else if (resultMessages.some(m => m.errorCode.indexOf('InvalidDefaultEmail') >=0)) {
-        errorTypeGroups.InvalidEmail.push(item)
-      } else if (resultMessages.some(m => m.message.indexOf('MetExitCriteria') >=0)) {
-        errorTypeGroups.MetExitCriteria.push(item)
-      } else if (resultMessages.some(m => m.errorCode.indexOf('CurrentlyWaitingInSameInteraction') >=0)) {
-        errorTypeGroups.CurrentlyWaitingInSameInteraction.push(item)
-      } else if (item.status==='ContactPreviouslyInSameInteraction') {
-        errorTypeGroups.ContactPreviouslyInSameInteraction.push(item)
+      if (resultMessages.some((m) => m.message.indexOf("excluded by List Detective") >= 0)) {
+        errorTypeGroups.ListDetective.push(item);
+      } else if (resultMessages.some((m) => m.message.indexOf("excluded by Suppression logic") >= 0)) {
+        errorTypeGroups.SuppressionLogic.push(item);
+      } else if (resultMessages.some((m) => m.message.indexOf("on the Global Unsubscribe List") >= 0)) {
+        errorTypeGroups.GlobalUnsubscribeList.push(item);
+      } else if (resultMessages.some((m) => m.message.indexOf("Email Address for this subscriber has invalid syntax") >= 0)) {
+        errorTypeGroups.InvalidEmail.push(item);
+      } else if (resultMessages.some((m) => m.errorCode.indexOf("InvalidDefaultEmail") >= 0)) {
+        errorTypeGroups.InvalidEmail.push(item);
+      } else if (resultMessages.some((m) => m.message.indexOf("MetExitCriteria") >= 0)) {
+        errorTypeGroups.MetExitCriteria.push(item);
+      } else if (resultMessages.some((m) => m.message.indexOf("MetGoalCriteria") >= 0)) {
+        errorTypeGroups.MetGoalCriteria.push(item);
+      } else if (resultMessages.some((m) => m.errorCode.indexOf("CurrentlyWaitingInSameInteraction") >= 0)) {
+        errorTypeGroups.CurrentlyWaitingInSameInteraction.push(item);
+      } else if (item.status === "ContactPreviouslyInSameInteraction") {
+        errorTypeGroups.ContactPreviouslyInSameInteraction.push(item);
       } else {
-        errorTypeGroups.Others.push(item)
+        errorTypeGroups.Others.push(item);
       }
     }
 
-    let countsMsgs = []
+    let countsMsgs = [];
     // let candidateErrTypes = ['Others', 'ListDetective', 'SuppressionLogic', 'InvalidEmail', 'MetExitCriteria', 'CurrentlyWaitingInSameInteraction', 'ContactPreviouslyInSameInteraction']
-    let candidateErrTypes = ['Others', 'ContactPreviouslyInSameInteraction'] // only show specific errors
-    let warningCounts = 0
-    candidateErrTypes.forEach(k => {
-      if (errorTypeGroups[k].length) {
-        countsMsgs.push(`${errorTypeGroups[k].length} ${k}`)
-        warningCounts += k
+    let candidateErrTypes = ["Others", "ContactPreviouslyInSameInteraction", "SuppressionLogic", "GlobalUnsubscribeList", "InvalidEmail"]; // only show specific errors
+    let warningCounts = 0;
+
+    candidateErrTypes.forEach((k) => {
+      if (errorTypeGroups[k].length >= 5) {
+        // only show when there's more than 5 errors
+        countsMsgs.push(`${errorTypeGroups[k].length} ${k}`);
+        warningCounts += k;
       }
-    })
+    });
 
     if (warningCounts) {
-      thisErrMsg += `- *${journeyName}* ${countsMsgs.join(", ")}`
+      thisErrMsg += `- *${journeyName}* ${countsMsgs.join(", ")}`;
 
-      let itemErrMsgs = []
+      let itemErrMsgs = [];
       for (let i = 0; i < errorTypeGroups.Others.length && i < 10; i++) {
-        itemErrMsgs.push("\t- "+genItemErrorMsg(errorTypeGroups.Others[i]))
-      } 
-      if (itemErrMsgs.length) {
-        thisErrMsg += "\n"+itemErrMsgs.join("\n")  
+        itemErrMsgs.push("\t- " + genItemErrorMsg(errorTypeGroups.Others[i]));
       }
-      
-      errors.push({journeyName: journeyName, items:items, message:thisErrMsg})
+      if (itemErrMsgs.length) {
+        thisErrMsg += "\n" + itemErrMsgs.join("\n");
+      }
+
+      errors.push({ journeyName: journeyName, items: items, message: thisErrMsg });
     }
-  })
+  });
 
-  return { errors }
-}
+  return { errors };
+};
 
-module.exports = check
+module.exports = check;
